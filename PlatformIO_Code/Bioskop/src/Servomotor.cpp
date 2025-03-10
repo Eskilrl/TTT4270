b95dd64dc7a1468e4f6a8c2c441ab6a1cc0c5af5
Elvin <elvingk@icloud.com>
2025-03-10 11:48:37 +0100

    Changed the library from Stepper.h to AccelStepper.h and two servomotors can be moved at once

--- PlatformIO_Code/Bioskop/src/Servomotor.cpp
+++ PlatformIO_Code/Bioskop/src/Servomotor.cpp
@@ -1,32 +1,52 @@
 #include <Arduino.h>
-#include <Stepper.h>
+#include <AccelStepper.h>
 
 const int stepsPerRevolution = 2048;  // change this to fit the number of steps per revolution
 
-// ULN2003 Motor Driver Pins
+// ULN2003 Motor Driver 
+
 #define IN1 19
 #define IN2 18
 #define IN3 5
 #define IN4 17
 
+#define IN5 27
+#define IN6 14
+#define IN7 12
+#define IN8 13
+
 // initialize the stepper library
-Stepper myStepper(stepsPerRevolution, IN1, IN3, IN2, IN4);
+AccelStepper stepper1(AccelStepper::HALF4WIRE, IN1, IN3, IN2, IN4);
+AccelStepper stepper2(AccelStepper::HALF4WIRE, IN5, IN7, IN6, IN8);
 
 void setup() {
-  // set the speed at 5 rpm
-  myStepper.setSpeed(10);
   // initialize the serial port
   Serial.begin(115200);
+  
+  // set the speed and acceleration
+  stepper1.setMaxSpeed(500);
+  stepper1.setAcceleration(100);
+
+  stepper2.setMaxSpeed(500/4);
+  stepper2.setAcceleration(100/4);
+  // set target position
+  stepper1.moveTo(stepsPerRevolution);
+  stepper2.moveTo(stepsPerRevolution/4);
+
+
 }
 
 void loop() {
-  // step one revolution in one direction:
-  Serial.println("clockwise");
-  myStepper.step(stepsPerRevolution);
-  delay(1000);
 
-  // step one revolution in the other direction:
-  Serial.println("counterclockwise");
-  myStepper.step(-stepsPerRevolution);
-  delay(1000);
+  // check current stepper motor position to invert direction
+  if (stepper1.distanceToGo() == 0 && stepper2.distanceToGo() == 0){
+    stepper1.moveTo(-stepper1.currentPosition());
+    stepper2.moveTo(-stepper2.currentPosition());
+
+    Serial.println("Changing direction");
+  }
+  // move the stepper motor (one step at a time)
+  stepper1.run();
+  stepper2.run();
+
 }
